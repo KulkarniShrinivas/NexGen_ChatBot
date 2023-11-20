d17fae07c83fb52802a6733fae8e1c46dd5a0dff
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-11-20 08:59:14 +0530

    Refresh gitignore

--- backend/dist/app.js
+++
@@ -1,9 +0,0 @@
-//all of the application qoutes will be handled 
-import express from 'express';
-import { config } from 'dotenv';
-config();
-const app = express();
-//middlewares
-app.use(express.json());
-export default app;
-//# sourceMappingURL=app.js.map
