9770ad1deeabc33eae382523df651bc9ea90d971
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-11-20 18:10:16 +0530

    created Signup route and validated

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,9 +1,55 @@
-export const getAllUsers = () => {
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+//hash is used to encrypt the password
+import { hash  } from 'bcrypt'
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
 
     //getting all the user from the database
     try {
-        
+        //get all users
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users});
+
     } catch (error) {
+        return res.status(400).json({message: "ERROR", cause: error.message});
+        
+    }
+};
+
+export const userSignup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    //getting all the user from the database
+    try {
+        //user signup
+        //will be getting data from client in below format 
+        // {
+        //     "name": "Shrinivas Kulkarni",
+        //     "email": "kulkarni@gmail.com",
+        //     "password": "12345"
+        // }
+
+        //destructure above 
+        const {name, email, password} = req.body;
+        //before creating new user we need to encrypt oassword using bcrypt so that we cant store password in backend
+        const hashedPassword = await hash(password, 10);
+        //now create brand new user
+        const user = new User({ name, email, password: hashedPassword });
+
+        //for saving new record in database 
+        await user.save();
+        return res.status(200).json({ message: "OK", id:user._id.toString()});
+
+    } catch (error) {
+        return res.status(400).json({message: "ERROR", cause: error.message});
         
     }
 };
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controller.js";
+import { getAllUsers, userSignup } from "../controllers/user-controller.js";
 
 
 const userRoutes = Router();
 
 //so after / we dont want to conroll all the request by this so I will be moving to controllers
 userRoutes.get("/", getAllUsers);
+userRoutes.post("/signup", userSignup);
 
 export default userRoutes;
