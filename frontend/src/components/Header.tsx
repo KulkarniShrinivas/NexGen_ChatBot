c1eee6e4fb3fafb2b4f516e2a460840e2610019a
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-11-30 22:50:15 +0530

    implemened Links to login logut and signup
    updated using state whteher user has been logged in or not if not will make him to login using navigation link and header for user to login logout

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,15 +2,56 @@ import React from 'react'
 import Logo from './shared/Logo';
 //using the heder will be using component of MUI
 import {  AppBar, Toolbar } from "@mui/material";
+import { useAuth } from '../context/AuthContext';
+import NavigationLink from './shared/NavigationLink';
 
-function Header() {
+
+const Header = () => {
+  const auth = useAuth();
   return (
-    <AppBar sx={{ bgcolor: "transparent", position:"static",boxShadow:"none"}}>
-        <Toolbar sx={{ display: "flex"}}>
-            <Logo />
-        </Toolbar>
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {/* is loggedin means user loged in */}
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
