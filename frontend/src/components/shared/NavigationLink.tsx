0d621bce3584b7a3128c5364c2d8efb430750d87
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-05 22:22:33 +0530

    updated

--- frontend/src/components/shared/NavigationLink.tsx
+++ frontend/src/components/shared/NavigationLink.tsx
@@ -12,7 +12,7 @@ type Props = {
 const NavigationLink = (props: Props) => {
   return (
     <Link
-      onClick={props.onClick}
+      
       className="nav-link"
       to={props.to}
       style={{ background: props.bg, color: props.textColor }}
