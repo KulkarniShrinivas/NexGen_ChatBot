7b30cd91192890f4aa8f26dd1f0db86bb19edd33
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-06 18:09:51 +0530

    created Login form

---
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {
+    name: string
+    type: string
+    label: string
+}
+
+const CustomizedInput = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomizedInput
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,54 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box 
+        padding={8} 
+        mt={8}
+        display={{ md: "flex", sm:"none", xs: "none" }}
+      >
+         <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box
+        display={'flex'}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        padding={2}
+        ml={"auto"}
+        mt={16}
+      >
+          <form
+            style={{
+              margin: "auto",
+              padding: "30px",
+              boxShadow: "10px 10px 20px #000",
+              borderRadius: "10px",
+              border: "none",
+            }}
+          >
+            <Box
+              sx={{
+                display:"flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+               variant="h4"
+               textAlign="center"
+               padding={2}
+               fontWeight={600}
+              >
+                Login
+              </Typography>
+            </Box>
+          </form>
+        </Box>
+
+    </Box>
   )
 }
 
