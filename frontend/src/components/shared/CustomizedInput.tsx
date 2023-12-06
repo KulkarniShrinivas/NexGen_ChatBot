9fc524b1ffd8435e850a8c0caf838269e953fae6
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-06 23:00:56 +0530

    updated Login form
    updated Login css and Login Typo

--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
+import { TextField } from '@mui/material';
+
+//This is customized textfield for LoginForm 
 
 type Props = {
     name: string
     type: string
     label: string
-}
+};
 
-const CustomizedInput = () => {
+const CustomizedInput = (props: Props) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <TextField
+    margin="normal" 
+        InputLabelProps={{style: {color: "white" }}}
+        name={props.name}
+        label={props.label} 
+        type={props.type} 
+        InputProps={{ 
+            style: { 
+                width: "400px", 
+                borderRadius: 10, 
+                fontSize:20, 
+                color:"white"
+            }}}
+    />
+  );
+};
 
-export default CustomizedInput
+export default CustomizedInput;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,7 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import {IoIosLogIn} from "react-icons/io"
+import CustomizedInput from '../components/shared/CustomizedInput'
 
 const Login = () => {
   return (
@@ -44,6 +46,27 @@ const Login = () => {
               >
                 Login
               </Typography>
+              <CustomizedInput type="email" name="email" label="Email" />
+              <CustomizedInput type="password" name="password" label="password" />
+              <Button 
+                type="submit"
+                sx={{
+                  px: 2,
+                  py: 1,
+                  mt: 2,
+                  width: "400px",
+                  borderRadius: 2,
+                  bgcolor: "#00fffc",
+                  ":hover": {
+                    bgcolor: "white",
+                    color: "black",
+                  },
+                }}
+
+                endIcon={<IoIosLogIn />}
+              >
+                Login
+              </Button>
             </Box>
           </form>
         </Box>
