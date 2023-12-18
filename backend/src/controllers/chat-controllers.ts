2e62462a226edf1de0fa8b872fbda13c5b6e8493
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-12-19 00:19:55 +0530

    backend operation with chat completion completed

--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { configureOpenAI } from '../config/openai-config.js';
+import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
+import { config } from 'dotenv';
 
 
 export const generateChatCompletion = async (
@@ -9,8 +12,8 @@ export const generateChatCompletion = async (
   ) => {
     //we want message from the user so from body will be accessing message from body
     const {message} = req.body;
-
-    //now we have message and we need to add validation
+    try {
+        //now we have message and we need to add validation
     //verify the details of the user
 
     const user = await User.findById(res.locals.jwtData.id);
@@ -19,19 +22,34 @@ export const generateChatCompletion = async (
     .json({message:"User not registred OR Token malfunctioned"});
 
 
-    //grab chat of the User 
-    const chats = user.chats.map(({ role, content}) => ({ role, content })); {/**This is the static message of the chat */}
-    chats.push({content:message, role:"user" }); {/* push the chats or send the chat from the user */}
-    user.chats.push({ content: message, role: "user" }); {/**So we need to store chats in main user objects */}
-    //above all will grab the chats of the user     
-
+    //grab chat of the User blow =>{/**This is the static message of the chat */}
+    const chats = user.chats.map(({ role, content }) => ({ 
+        role,
+        content,
+      })) as ChatCompletionRequestMessage[];
+      chats.push({ content: message, role: "user" }); {/* push the chats or send the chat from the user */}
+      user.chats.push({ content: message, role: "user" }); {/**So we need to store chats in main user objects */}
+      //above all will grab the chats of the user     
+  
+    
 
 
 
     //Send all chats with new one to openAi API
+    const config = configureOpenAI();
+    const openai = new OpenAIApi(config);
+    // get latest response
+    const chatResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chats,
+    });
+    user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        return res.status(200).json({ chats: user.chats });
 
-    
-    //get latest response
-
-
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+  
   };
