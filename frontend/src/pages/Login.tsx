c682f87931a578f622978de16f8d14a271bdce93
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-01-29 01:36:27 +0530

    updated login if user is logged in he will be redirected to the chat page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -26,6 +26,15 @@ const Login = () => {
       return navigate("/chat");
     }
   }, [auth]);
+
+
+  useEffect(() => {
+    if(auth?.user) {
+      return navigate("/chat")
+    }
+  }, [auth]);
+
+
   return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
