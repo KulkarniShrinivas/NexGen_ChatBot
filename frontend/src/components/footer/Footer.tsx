8eb673a8371bc6e087e6912e3fce95e5ff7897ff
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-03-07 01:42:33 +0530

    updated Footer page

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import {Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer>
+      <div 
+        style={{
+          width: "100",
+          padding: 20,
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 100,
+        }}>
+          <p style={{
+            fontSize: "30px", textAlign: "center" 
+          }}>
+            built by
+             <span>
+              <Link 
+                style={{ color: "white"}}
+                className="nav-link"
+                 to={'https://github.com/KulkarniShrinivas'}
+                 >KulkarniShrinivas</Link>
+              </span> 
+          </p>
+
+      </div>
+    </footer>
+  );
+};
 
 export default Footer
