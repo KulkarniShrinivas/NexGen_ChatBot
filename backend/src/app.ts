1663b06727770d950a359f97fca3e65c0b298731
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-03-08 21:52:13 +0530

    updated links

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,7 @@ const app = express();
 
 //middlewares
 app.use(cors({ origin: "https://nextgen-chatbot.netlify.app", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.options('*', cors({ credentials: true, optionsSuccessStatus: 204 }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
